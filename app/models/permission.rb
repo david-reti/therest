f1f7d493c75d5808830c4b8c9650850e1d8a4020
David Reti <development@davidreti.net>
2022-11-15 14:26:18 -0500

    Fix permission types

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,5 +2,5 @@ class Permission < ApplicationRecord
     include Immutable
     has_and_belongs_to_many :roles
     enum permission_type:  [ :create_article, :edit_own_article, :edit_all_articles, :delete_own_article, :delete_all_articles,
-                             :create_comment, :edit_own_comment, :delete_own_comment, :delete_all_comments   ]
+                             :create_comment, :edit_own_comment, :edit_all_comments, :delete_own_comment, :delete_all_comments   ]
 end
